c7c1ff80b3650333f3d9ff2d13c27631d21b4948
Abhishek7379222 <129316418+Abhishek7379222@users.noreply.github.com>
2025-02-08 15:41:52 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -83,14 +83,11 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME", "mydb"),
-        'USER': os.getenv("DB_USER", "root"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "password"),
-        'HOST': os.getenv("DB_HOST", "127.0.0.1"),  # Default to localhost
-        'PORT': os.getenv("DB_PORT", "3306"),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
